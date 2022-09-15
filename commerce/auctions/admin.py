4321746cb0a849de17efcd9826e195906bba4e8b
Wojciech Brożek <wobrozek@gmail.com>
2022-09-15 23:03:22 +0200

    add model

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Comment, Bid ,Auction ,User
 
-# Register your models here.
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Auction)
+admin.site.register(User)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    img = models.CharField(max_length=512, blank=True)
+
+class Comment(models.Model):
+    text = models.CharField(max_length=2048)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comment")
+
+class Bid(models.Model):
+    price = models.FloatField()
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid")
+    auction = models.ForeignKey(Auction ,on_delete=models.CASCADE, related_name="bid")
